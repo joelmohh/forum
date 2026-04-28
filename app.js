6881f34be7ca8f443953783a39ec70b644396f2a
Joel Moh <me@joelmo.dev>
2026-04-27 20:25:28 -0600

    commit

--- app.js
+++ app.js
@@ -18,7 +18,13 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 
 
 const mainRoutes = require('./routes/main.routes');
+const authRoutes = require('./routes/auth.routes');
+const apiRoutes = require('./routes/api.routes');
+
 app.use('/', mainRoutes);   
+app.use('/auth', authRoutes);
+app.use('/api', apiRoutes); 
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
